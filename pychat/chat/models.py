0a048cd1c4f44f3c271a4e57b000b80f1bb47331
Larissa Queiroz <larissa.queiroz@nepen.org.br>
2024-06-01 11:59:56 -0300

    create models

--- pychat/chat/models.py
+++ pychat/chat/models.py
@@ -12,13 +12,13 @@ class Chat(Base):
     initial_datetime = models.DateTimeField(null=True, blank=True)
 
 class User(Base):
-    chat = models.ForeignKey(Chat, on_delete=models.DO_NOTHING)
+    chat_id = models.ForeignKey(Chat, on_delete=models.DO_NOTHING)
     username = models.CharField(max_length=15, null=False, blank=False)
     password = models.CharField(max_length=12, null=False, blank=False)
     email = models.CharField(max_length=50, null=False, blank=False)
 
 class Message(Base):
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
     content = models.CharField(null=False, max_length=500, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime_sent = models.DateTimeField(null=True, blank=True, default=None)
